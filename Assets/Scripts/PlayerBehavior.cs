a1c8a23fafa0005cf116d5d5a44f593940846f6c
Tee80tee <56715073+Tee80tee@users.noreply.github.com>
2020-02-24 21:32:13 +0800

    Ratima's first commit

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    GameObject gm;
+    GridBehavior gridBehaviorCode;
+    GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GameController");
+       gridBehaviorCode = gm.GetComponent<GridBehavior>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        parent = this.transform.parent.gameObject;
+        parent.GetComponent<GridStat>().status = 2;
+    }
+
+    
+    //playerMove receive input from player
+    public void  PlayerMove(int endX,int endY)
+    {
+        gridBehaviorCode.findDistance = true;
+        Debug.Log("doing something");
+        int currentX;
+        int currentY;
+        //findcurrent
+        currentX = parent.GetComponent<GridStat>().x;
+
+        currentY = parent.GetComponent<GridStat>().y;
+
+        gridBehaviorCode.RunThePath(currentX, currentY, endX, endY);
+        
+    }
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    public Button startButton;
+    public InputField x;
+    
+    public InputField y;
+    public GameObject player;
+    
+    void Start()
+    {
+        
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Button btn = startButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+       int numX = int.Parse(x.text);
+        int numY = int.Parse(y.text);
+        player.GetComponent<PlayerBehavior>().PlayerMove(numX,numY);
+    }
+}
