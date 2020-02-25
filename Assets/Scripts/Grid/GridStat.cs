654479f756f82b816a0170ea2cdf1dfca348d8dc
pnguyen95-collab <56715091+pnguyen95-collab@users.noreply.github.com>
2020-02-25 13:05:09 +0800

    Stats System (Base)
    By Phu

---
+++ Assets/Scripts/Grid/GridStat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridStat : MonoBehaviour
+{
+    public int visit = -1;
+    public int x = 0;
+    public int y = 0;
+    public bool standable = true;
+
+    public int status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (status)
+        {
+            case 0:
+                this.GetComponent<Renderer>().material.color = Color.white;
+                break;
+            case 1:
+                this.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 2:
+                this.GetComponent<Renderer>().material.color = Color.green;
+                break;
+
+        }             
+    }
+}
---
+++ Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat 
+{
+    [SerializeField]
+    private int baseValue;
+
+    //Get the current value of stats after modifiers
+    public int GetValue ()
+    {
+        int finalValue = baseValue;
+
+        // Blah blah stat modifiers from Attachments
+
+        return finalValue;
+    }
+}
